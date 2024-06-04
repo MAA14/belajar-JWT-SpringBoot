336f8b125ce014e5cd175b60af1b5cf22a789f7a
MAA14 <muhammadahya252@gmail.com>
2024-06-04 20:21:09 +0700

    Update: create UserRepository

---
+++ src/main/java/maa/belajar_JWT_springboot/user/UserRepository.java
@@ -0,0 +1,12 @@
+package maa.belajar_JWT_springboot.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
