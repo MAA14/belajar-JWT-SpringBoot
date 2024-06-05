c0d9dddfdf7de7d6c9c920203f865ad8abffabe8
MAA14 <muhammadahya252@gmail.com>
2024-06-05 08:39:24 +0700

    Update : write documentation in JwtService for extract claims method

--- src/main/java/maa/belajar_JWT_springboot/config/JwtService.java
+++ src/main/java/maa/belajar_JWT_springboot/config/JwtService.java
@@ -15,14 +15,24 @@ public class JwtService {
     // Ini keynya pake Random Generator dari "https://generate-random.org/encryption-key-generator?count=1&bytes=256&cipher=aes-256-cbc&string=&password="
     private static final String SECRET_KEY = "6imUScP5CgJImn6GUNyj+akb4hJmJ883JBEchvQ1V5fag8G3tD5ZnPJSV+Ubyr2czN+F11lv1BfISoS4nMccDZlKqtzLbSp4Bqzfaovtffz10UyvXeoPhQ3VOFOoL2pkD5CzO1GVsVvipA5gpkrrIAmEjPnZalpEQQ8Xuw0JSiL6ogt6tyI5qAHCoJNHgsi0EHfVo07hVIWXSgFC44PzkaW2YtSe3eRft9N2AZMR21xsqyHGrzZ5Yce+0zmiSVTydt9V1emz1csi22NjbWlYmYk4ovDNk/brNDOpcK1oLfVoRDaFgkX/TOKjYfyhfbyNql0tHgysL4md8YehSs+9OcTcNOPEzeX4DhrnWzDueqQ=";
 
+    /** TO DO TOMORROW belajar Claims::getSubject dan Function<Claims, T> dari java.util.function */
     public String extractUsername(String jwtToken) {
-        return extractClaim(jwtToken, Claims::getSubject);
+        return extractClaim(
+                jwtToken, /** Input: String = "token-jwt" */
+                Claims::getSubject /** Input: Claims::method() ||  (Claims claims) -> claims.method() */
+                // Parameter (Claims claims) didapat dari logic method extractClaim, jadi kita hanya perlu memanggil methodnya
+                // Kita juga bisa melakukan filtering atau logic tambahan pada object Claims di method ini, dengan syarat harus punya return data
+        );
     }
 
     /** Method untuk mengekstrak satu data aja dalam JWT -> data = claims */
-    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(jwtToken);
-        return claimsResolver.apply(claims);
+    public <T> T extractClaim(
+            String jwtToken, /** Input: String = "token-jwt" */
+            // Parameter ini harus berupa fungsi dengan Param input harus object Claims dan harus punya return <T>
+            Function<Claims, T> claimsResolver  /** Input: method(Claims claims) -> claims.method() */
+    ) {
+        final Claims claims = extractAllClaims(jwtToken); // Buat object Claims
+        return claimsResolver.apply(claims); // Aktifkan method yang ada pada parameter claimsResolver
     }
 
     /** Method untuk mengekstrak seluruh data dalam JWT token -> data = Claims */
@@ -31,7 +41,7 @@ public class JwtService {
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
-                .parseClaimsJws(jwtToken)
+                .parseClaimsJws(jwtToken) // Menguraikan Claims JWS
                 .getBody(); // Disini kita bisa getHeader dan signature
     }
 
